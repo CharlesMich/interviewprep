30a16ea631800c5f9f00a5366beadd65e63f6b2c
CharlesMich <charleedongo@hotmail.com>
2023-09-14 17:03:17 -0500

    sep 14

--- phase1.js
+++ phase1.js
@@ -441,3 +441,16 @@ var longestCommonPrefix1 = function(strs) {
     }
     return longestPrefix
 };
+
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the 
+// integers in the range [1, n] that do not appear in nums.
+
+var findDisappearedNumbers = function(nums) {
+    let newArr = []
+    for (let i = 1; i < nums.length+1; i++ ){
+        if(!nums.includes(i)){
+            newArr.push(i)
+        }
+    }
+    return newArr
+};
