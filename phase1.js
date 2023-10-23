c612be33c3a86ad88fd9267984f9bceb57cc1113
CharlesMich <charleedongo@hotmail.com>
2023-10-23 10:13:19 -0500

    oct 23

--- phase1.js
+++ phase1.js
@@ -472,3 +472,25 @@ var nextGreatestLetter = function(letters, target) {
     }
     return letters[0]
 };
+
+// Happy number
+var isHappy = function (n) {
+    let numStr = n.toString();
+    let numArr = numStr.split("");
+    let sum = 0;
+    for (let i = 0; i < numArr.length; i++) {
+      sum = sum + Math.pow(numArr[i], 2);
+      if (i === numArr.length - 1) {
+        numStr = sum.toString();
+        numArr = numStr.split("");
+        if (sum === 1 || sum===32) {
+          return true;
+        }
+        if ((sum & (sum - 1)) === 0) {
+          return false;
+        }
+        sum = 0;
+        i = -1;
+      }
+    }
+  };
