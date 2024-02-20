9e5388f2ce5abcb7219aa130afc8947006c802be
CharlesMich <charleedongo@hotmail.com>
2024-02-20 16:58:10 -0600

    feb 20

--- phase1.js
+++ phase1.js
@@ -549,5 +549,25 @@ var largestPerimeter = function(nums) {
 };
 console.log(largestPerimeter(polynums))
 
+// group anagrams
+strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
+var groupAnagrams = function(strs) {
+   let obj = {}, arr =[]
+   for (let i = 0;i < strs.length; i++){
+    let index = strs[i].split('').sort().join('')
+    if(obj[index]){
+        obj[index].push(strs[i])
+    }else {
+        obj[index] = [strs[i]]
+    }
+}
+for (let item in obj){
+    arr.push(obj[item])
+}
+   return arr;
+}
+
+console.log(groupAnagrams(strs))
 
