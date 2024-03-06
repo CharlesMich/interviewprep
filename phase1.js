97d61488d6afba01556f6eeb3f32b44c72696c09
CharlesMich <charleedongo@hotmail.com>
2024-03-05 20:23:38 -0600

    mar 5

--- phase1.js
+++ phase1.js
@@ -735,4 +735,18 @@ var combinationSum = function(candidates, target) {
 // Input: candidates = [2,3,6,7], target = 7
 // Output: [[2,2,3],[7]]
 
-console.log(combinationSum([2,3,6,7],  7))
+// console.log(combinationSum([2,3,6,7],  7))
+
+var lengthOfLongestSubstring = function(s) {
+    const lastSeen = {};
+ let longest = [0,1];
+ let startIdx = 0;
+
+ for(let i=0;i<s.length;i++){
+   const char = s[i];
+   if( char in lastSeen)  startIdx = Math.max(startIdx, lastSeen[char] + 1);
+   if(longest[1]-longest[0] < i+ 1 - startIdx) longest = [startIdx,i+1];
+    lastSeen[char] = i;
+ }
+  return s.slice(longest[0],longest[1]).length;
+};
