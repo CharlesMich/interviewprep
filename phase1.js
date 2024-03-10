3032b886ccf3b79e86e9f7c181de9dca76c53d3f
CharlesMich <charleedongo@hotmail.com>
2024-03-09 23:17:04 -0600

    mar 9

--- phase1.js
+++ phase1.js
@@ -886,3 +886,33 @@ var divide = function(dividend, divisor, sum=0) {
      if ( dividend > Math.abs(divisor)) sum = sum + 1;
      return divide(dividend-Math.abs(divisor), divisor, sum)
  };
+
+//  three sum
+var threeSum = function(nums) {
+    const res=[]
+   nums.sort((a,b)=>a-b)
+   for(let i=0;i<nums.length;i++){
+       if(i>0 && nums[i] == nums[i-1]){
+           continue;
+       }
+       let left=i+1;
+       let right = nums.length-1;
+
+       while(left<right){
+           let threesum = nums[i]+nums[left]+nums[right]
+           if(threesum>0){
+               right--;
+           }else if(threesum<0){
+               left++;
+           }else{
+               res.push([nums[i],nums[left],nums[right]])
+               left++;
+
+               while(nums[left] == nums[left-1] && left<right){
+                   left++;
+               }
+           }
+       }
+   }
+   return res
+};
