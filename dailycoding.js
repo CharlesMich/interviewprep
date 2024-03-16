30f2c1b459fce9ed98cbbf8f8c60c3e1f141fa29
CharlesMich <charleedongo@hotmail.com>
2024-03-16 12:18:55 -0500

    mar 16

--- dailycoding.js
+++ dailycoding.js
@@ -38,3 +38,16 @@ function findLowest(arr){
     return lowest
 }
 console.log(findLowest([3, 4, -1, 1]))
+
+// Good morning! Here's your coding interview problem for today.
+
+// This problem was asked by Jane Street.
+// cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+function cons(a, b){
+    function pair(f){
+        return f(a, b)
+    }
+    return pair
+
+}
