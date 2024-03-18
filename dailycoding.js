5a95006ecd7a919a68a0ff7a9b2bbf38d8ab38ef
CharlesMich <charleedongo@hotmail.com>
2024-03-18 13:31:38 -0500

    mar 18

--- dailycoding.js
+++ dailycoding.js
@@ -37,7 +37,7 @@ function findLowest(arr){
     }
     return lowest
 }
-console.log(findLowest([3, 4, -1, 1]))
+// console.log(findLowest([3, 4, -1, 1]))
 
 // Good morning! Here's your coding interview problem for today.
 
@@ -51,3 +51,53 @@ function cons(a, b){
     return pair
 
 }
+
+console.log(cons(3,4))
+// Implement car and cdr
+function car(xyz){
+    return xyz
+}
+
+function cdr(xyz){
+    return xyz
+}
+
+console.log(car(cons(3, 4)))
+
+
+// Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded. For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'. You can assume that the messages are decodable. For example, '001' is not allowed.
+let code = {
+    a:1,
+    b:2,
+    c:3,
+    d:4,
+    e:5,
+    f:6,
+    g:7,
+    h:8,
+    i:9,
+    j:10,
+    k:11,
+    l:12,
+    m:13,
+    n:14,
+    o:15,
+    p:16,
+    q:17,
+    r:18,
+    s:19,
+    t:20,
+    u:21,
+    v:22,
+    w:23,
+    x:24,
+    y:25,
+    z:26
+}
+function decode(num){
+    const arr = num.toString().split("")
+    const newArr = [];
+    
+    return arr;
+}
+console.log(decode(121))
