43474843b9c548ddd9b8b220d7f96eb866829db7
CharlesMich <charleedongo@hotmail.com>
2024-03-28 13:21:44 -0500

    mar 28

--- phase1.js
+++ phase1.js
@@ -1039,3 +1039,41 @@ var convertToTitle = function(columnNumber) {
         }
     }
     console.log(longestPalindrome('ababad'))
+
+    // Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+//     Input: nums = [-1,2,1,-4], target = 1
+// Output: 2
+// Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+var threeSumClosest = function(nums, target) {
+    let num;
+    let matrix = [];
+    for (let i = 0; i < nums.length-2; i++){
+        console.log(nums[i])
+        for (let j = i + 1; j < nums.length-1; j++ ){
+            
+            for (let k = j + 1; k < nums.length; k++){
+                matrix.push([nums[i], nums[j], nums[k]])
+            }
+        }
+    }
+    for ( let submatrix of matrix){
+        let sum = 0;
+        for(let i =0; i < submatrix.length; i++){
+            sum += submatrix[i]
+        }
+        if (sum < num){
+            num = sum;
+        }
+    }
+};
+console.log(threeSumClosest([-1,2,1,-4], 1))
+
+function arrLength(num1){
+    let i = 0;
+    while ( i < num1.length+1){
+        console.log(num1[i])
+        i++
+    }
+}
+console.log(arrLength([1,2,3,4]))
