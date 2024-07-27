743034b976f749c2ce513b0ca5467967949f350b
CharlesMich <charleedongo@hotmail.com>
2024-07-26 23:16:02 -0500

    july 26

--- grind75.js
+++ grind75.js
@@ -446,3 +446,5 @@ var longestOnes = function(nums, k) {
     }
     return max
 };
+
+// Given a binary array nums, you should delete one element from it. Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
