bc0b5b3f0c35c401d9e4bb91c0d2ed0f840f7775
CharlesMich <charleedongo@hotmail.com>
2024-07-30 23:40:46 -0500

    july 30

--- grind75.js
+++ grind75.js
@@ -465,3 +465,22 @@ var longestSubarray = function(nums) {
     }, 0)
     return result
  };
+
+//  There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0. You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+ var largestAltitude = function(gain) {
+    let sum = 0;
+     let alt = [];
+     let max;
+     for(let i=0;i<=gain.length;i++){
+       alt.push(sum);
+       sum = sum + gain[i];
+     }
+     max = alt[0];
+     for(let j=0;j<alt.length;j++){
+         if(max < alt[j]){
+             max=alt[j];
+         }
+     }
+     return max;
+     }
